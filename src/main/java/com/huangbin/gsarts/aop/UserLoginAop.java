3cbbaa1a4fd1c14a857e9b586b60843dd0c3941e
huangbin <876301469@qq.com>
2018-02-12 08:32:08 +0800

    c

--- src/main/java/com/huangbin/gsarts/aop/UserLoginAop.java
+++ src/main/java/com/huangbin/gsarts/aop/UserLoginAop.java
@@ -1,27 +1,13 @@
-package com.yhb.aop;
+package com.huangbin.gsarts.aop;
 
 
-import com.yhb.domain.log.UserLog;
-import com.yhb.domain.sysInfo.SysParams;
-import com.yhb.domain.user.User;
-import com.yhb.service.sysInfo.SysParamsService;
-import com.yhb.service.userLog.UserLogService;
-import com.yhb.vo.ReturnObject;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-import java.util.Date;
 
 /**
  * 用户日志aop
@@ -31,11 +17,13 @@ import java.util.Date;
 @Order(1)
 @Slf4j
 public class UserLoginAop {
-    @Autowired
-    UserLogService userLogService;
 
-    @Autowired
-    SysParamsService sysParamsService;
+
+//    @Autowired
+//    UserLogService userLogService;
+//
+//    @Autowired
+//    SysParamsService sysParamsService;
 
     /**
      * @param joinPoint   结合点
@@ -47,26 +35,26 @@ public class UserLoginAop {
         //设置日志开关
 
         //从系统参数表中查询是否需要记录访问日志
-        SysParams sysParams = sysParamsService.findByParamName("DATA_TRANSFORM_LOG_STATUS");
-        String logaable = sysParams.getParamValue();
-        if (!logaable.equals("1")) {
-            log.info("是否记录用户登录日志？" + logaable);
-            return;
-        }
-
-        String userName = (String) joinPoint.getArgs()[1];
-        ReturnObject returnObject = (ReturnObject) loginResult;
-        boolean result = returnObject.getResult();
-        Object[] args = joinPoint.getArgs();
-        HttpServletRequest request = (HttpServletRequest) args[0];
-        String ip = request.getRemoteHost();
-        UserLog userLog = new UserLog();
-        userLog.setUserName(userName);
-        userLog.setLoginIp(ip);
-        userLog.setOperation("登录");
-        userLog.setOperationTime(new Date());
-        userLog.setStatus(result ? "登录成功" : "登录失败");
-        userLogService.createUserLog(userLog);
+//        SysParams sysParams = sysParamsService.findByParamName("DATA_TRANSFORM_LOG_STATUS");
+//        String logaable = sysParams.getParamValue();
+//        if (!logaable.equals("1")) {
+//            log.info("是否记录用户登录日志？" + logaable);
+//            return;
+//        }
+//
+//        String userName = (String) joinPoint.getArgs()[1];
+//        ReturnObject returnObject = (ReturnObject) loginResult;
+//        boolean result = returnObject.getResult();
+//        Object[] args = joinPoint.getArgs();
+//        HttpServletRequest request = (HttpServletRequest) args[0];
+//        String ip = request.getRemoteHost();
+//        UserLog userLog = new UserLog();
+//        userLog.setUserName(userName);
+//        userLog.setLoginIp(ip);
+//        userLog.setOperation("登录");
+//        userLog.setOperationTime(new Date());
+//        userLog.setStatus(result ? "登录成功" : "登录失败");
+//        userLogService.createUserLog(userLog);
     }
 
 
--- src/main/java/com/huangbin/gsarts/aop/UserOperationAop.java
+++ src/main/java/com/huangbin/gsarts/aop/UserOperationAop.java
@@ -1,21 +1,13 @@
-package com.yhb.aop;
+package com.huangbin.gsarts.aop;
 
 
-import com.yhb.domain.log.UserLog;
-import com.yhb.service.userLog.UserLogService;
-import com.yhb.vo.ReturnObject;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.Date;
 
 /**
  * 用户日志aop
@@ -24,8 +16,8 @@ import java.util.Date;
 @Aspect
 @Order(2)
 public class UserOperationAop {
-    @Autowired
-    UserLogService userLogService;
+//    @Autowired
+//    UserLogService userLogService;
     protected Log log = LogFactory.getLog(this.getClass());
 
     /**
@@ -33,23 +25,23 @@ public class UserOperationAop {
      * @param object    操作结果
      *                  执行save delete时执行
      */
-    @AfterReturning(value = "execution(* com.yhb.service.*.save(..))|| execution(* com.yhb.service.*.delete(..))", returning = "object")
+    @AfterReturning(value = "execution(* com.huangbin.gsarts.service.*.save(..))|| execution(* com.huangbin.gsarts.service.*.delete(..))", returning = "object")
     public void doAfterReturningAdvice(JoinPoint joinPoint, Object object) {
 
 
 
-        String userName = (String) joinPoint.getArgs()[1];
-        ReturnObject returnObject = (ReturnObject) object;
-        boolean result = returnObject.getResult();
-        Object[] args = joinPoint.getArgs();
-        HttpServletRequest request = (HttpServletRequest) args[0];
-        String ip = request.getRemoteHost();
-        UserLog userLog = new UserLog();
-        userLog.setUserName(userName);
-        userLog.setLoginIp(ip);
-        userLog.setOperation("数据操作");
-        userLog.setOperationTime(new Date());
-        userLog.setStatus(result ? "操作成功" : "操作失败");
-        userLogService.createUserLog(userLog);
+//        String userName = (String) joinPoint.getArgs()[1];
+//        ReturnObject returnObject = (ReturnObject) object;
+//        boolean result = returnObject.getResult();
+//        Object[] args = joinPoint.getArgs();
+//        HttpServletRequest request = (HttpServletRequest) args[0];
+//        String ip = request.getRemoteHost();
+//        UserLog userLog = new UserLog();
+//        userLog.setUserName(userName);
+//        userLog.setLoginIp(ip);
+//        userLog.setOperation("数据操作");
+//        userLog.setOperationTime(new Date());
+//        userLog.setStatus(result ? "操作成功" : "操作失败");
+//        userLogService.createUserLog(userLog);
     }
 }
